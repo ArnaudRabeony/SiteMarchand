8b781df5621e9d52934c18e081ff13632fa76245
ArnaudRabeony <ar_rabeony@hotmail.fr>
2016-03-16 00:04:36 +0100

    Ajout CSS/menu adapté

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     include('fonctions.php');
     protectPostGet();
 
-    $containedPage = isset($_GET['page']) && trim($_GET['page'])!="" ? $_GET['page'] : "page_co";
+    $containedPage = isset($_GET['page']) && trim($_GET['page'])!="" ? $_GET['page'] : "football";
 ?>
 
 <!DOCTYPE html>
@@ -29,6 +29,7 @@
 
     <!-- Custom CSS -->
     <link href="css/shop-homepage.css" rel="stylesheet">
+    <link href="css/global.css" rel="stylesheet">
 
     <!-- Font Awesome -->
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
@@ -43,52 +44,8 @@
 </head>
 
 <body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Site Marchand</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        <!-- /.container -->
-        </div>
-        <div id="customerContainer" class="navbar-text navbar-right col-xm-2 col-sm-2 col-md-2" style="position:relative;bottom: 50px;margin-bottom: -45px;height:30px;">
-            <div class="dropdown">
-                <a href="#" style="float:right;margin-right: 20px"class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><i class="fa fa-user" style="color:#969696"></i></a>
-                <ul class="dropdown-menu" style="margin-top:20px;">
-                    <li><a href="#">Mon compte</a></li>
-                    <li><a href="#">Mes commandes <span id="currentCommand"></span></a></li>
-                    <li><a href="#">Mon panier</li>
-                    <li role="separator" class="divider"></li>
-                    <li><a href="index.php?page=page_co">Se déconnecter</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    
-    <div id="categories" class="col-md-2" style="position:fixed;margin-left:90px;">
+    <?php include('menu.php'); ?>
+    <div id="categories" class="col-md-2">
         <p class="lead">Shop Name</p>
         <div class="list-group">
             <a href="index.php?page=football" class="list-group-item">Football</a>
@@ -98,7 +55,7 @@
     </div>   
     <!-- Page Content -->
     <div class="container">
-            <div id="containedPage" class="col-md-9" style="background-color:red;float:right">
+            <div id="containedPage" class="col-md-9">
                 <?php include($containedPage.'.php'); ?>
             </div>
 
---
+++ menu.php
@@ -0,0 +1,95 @@
+<?php 
+
+
+$connected = isset($_SESSION['id']) ? true : false;
+$isAdmin = isset($_SESSION['type']) && $_SESSION['type']=="admin" ? true : false;
+// $connected=true;
+// $isAdmin=true;
+
+ ?>
+
+<!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Site Marchand</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        <!-- /.container -->
+        </div>
+        
+        <div id="customerContainer" class="navbar-text navbar-right col-xm-2 col-sm-2 col-md-2" style="">
+            
+        <?php 
+
+                
+        if($connected)
+        {   
+            echo '<div class="dropdown">
+                <button class="btn btn-default dropdown-toggle" type="button" id="customerMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                    <i class="fa fa-user"></i>Prénom N.
+                    <span class="caret"></span>
+                </button>';
+
+            if($isAdmin)
+            {
+                /*
+                 * gestion des produits : liste/ajout/suppression des produits des différentes catégories
+                 * gestion des utilisateurs : liste/modification/suppression des utilisateurs
+                 */
+
+                echo '<ul class="dropdown-menu">
+                    <li><a href="#">Gestion des produits</a></li>
+                    <li><a href="#">Gestion des utilisateurs</a></li>
+                    <div class="divider"></div>
+                    <li><a href="index.php?page=page_deco">Se déconnecter</a></li>
+                </ul>
+                </div>
+                </div>';
+            }
+            else
+            {
+                echo '<ul class="dropdown-menu">
+                    <li><a href="#">Mon compte</a></li>
+                    <li><a href="#">Mes commandes <span id="currentCommand"></span></a></li>
+                    <li><a href="#">Mon panier</li>
+                    <div class="divider"></div>
+                    <li><a href="index.php?page=page_deco">Se déconnecter</a></li>
+                </ul>
+                </div>
+                </div>';
+            }                    
+        }
+        else
+        {
+            echo '
+            <div id="connexionButtonDiv" class="nav navbar-right col-xm-2 col-sm-2 col-md-2">
+                <button id="connexionButton" class="btn btn-default"><a href="index.php?page=page_co">Connexion</a></button>
+            </div>
+            </div>';
+        }
+
+        ?>
+        
+    </nav>
